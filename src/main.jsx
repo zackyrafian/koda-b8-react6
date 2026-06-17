ad86b2b84729434139b9dc7bf303f3d88651f201
Zacky Rafian Fawwauzy <imftcy@gmail.com>
2026-06-17 09:52:41 +0700

    chore: init vite + react

---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+import { createBrowserRouter, RouterProvider } from 'react-router'
+
+
+const router = createBrowserRouter([
+  {
+    
+  }
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    {/* <App />*/}
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
